8c350e8316bdf2dd19bc3e39ef8d22dfa749880f
KirubhaShanthini <145562040+kirubha412@users.noreply.github.com>
2023-11-27 11:34:26 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,47 @@
+<!-- Mobile App Integrations -->
+<?php require("header.php");?>
+<div class="container mobile-app-integrations">
+            <h5>Integrations</h5>
+            <h1>Mobile App Integrations</h1>
+            <div class="design-code-testing">
+                <div class="row py-3 justify-content-center">
+                    <section class="testing-box col-lg-4 col-md-6 col-sm-12 my-3">
+                        <div class="design">
+                            <div class="design-content">
+                                <div class="icn-head">
+                                    <div class="icon-set"><i class="fas fa-palette"></i></div>
+                                    <h4>Design</h4>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Curabitur id libero.</p>
+                                <a href="#">Read More</a> 
+                            </div>
+                        </div>
+                        </section>
+                        <section class="testing-box col-lg-4 col-md-6 col-sm-12 my-3">
+                            <div class="code">
+                                <div class="code-content">
+                                <div class="icn-head">
+                                    <div class="icon-set"><i class="fas fa-code"></i></div>
+                                    <h4>Code</h4>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Curabitur id libero.</p>
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="testing-box col-lg-4 col-md-6 col-sm-12 my-3">
+                            <div class="testing">
+                                <div class="testing-content">
+                                <div class="icn-head">
+                                    <div class="icon-set"><i class="far fa-hand-point-up"></i></div>
+                                    <h4>Testing</h4>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Curabitur id libero.</p>
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </section>
+                </div>
+            </div>
+        </div>
+        <?php include("footer.php");?>
